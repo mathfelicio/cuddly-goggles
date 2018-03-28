9b58a48b8feef74dbf758d36ae30ca44678eff59
Matheus Felicio <matheus.felicio37@gmail.com>
2018-03-28 13:05:25 -0300

    Navegação na barra do topo

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -15,7 +15,8 @@
 
       <div class="nav-scroller py-1 mb-2">
         <nav class="nav d-flex justify-content-between">
-          <a class="p-2 text-muted" href="#">Blog</a>
-          <a class="p-2 text-muted" href="#">Posts</a>
+          <a href="/blog/public/posts" class="p-2 text-muted" >Blog</a>
+          <a href="/blog/public/posts" class="p-2 text-muted">Posts</a>
+          <a href="/blog/public/posts/create" class="p-2 text-muted">Create new Post</a>
         </nav>
       </div>
